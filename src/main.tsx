c9176762dac86996c28587deccecec9363be0217
Laurie Roy <laurieroy.dev@gmail.com>
2023-07-10 13:55:53 -0500

    - set up Redux toolkit store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+import App from "./App.tsx";
+import "./index.css";
+import { api } from "./state/api.ts";
+
+export const store = configureStore({
+  reducer: { [api.reducerPath]: api.reducer },
+  middleware: (getDefault)=> getDefault().concat(api.middleware),
+
+})
+setupListeners(store.dispatch);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
