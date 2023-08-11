420db36a0ecfe58ef736bc3f704370b2d37693b6
Laurie Roy <laurieroy.dev@gmail.com>
2023-08-11 16:26:27 -0500

    Add getProducts EP

--- src/scenes/dashboard/Row2.tsx
+++ src/scenes/dashboard/Row2.tsx
@@ -1,7 +1,10 @@
 import DashboardBox from "@/components/DashboardBox";
+import { useGetProductsQuery } from "@/state/api";
 
 type Props = {};
 const Row2 = (props: Props) => {
+  const { data } = useGetProductsQuery();
+  console.log(data);
   return (
     <>
       <DashboardBox gridArea="d">"d"</DashboardBox>
