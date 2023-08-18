db49f4529176b4310e7064efdd3a7eaaf46b3753
Laurie Roy <laurieroy.dev@gmail.com>
2023-08-18 10:50:08 -0500

    Add DataGrid

--- src/scenes/dashboard/Row3.tsx
+++ src/scenes/dashboard/Row3.tsx
@@ -1,10 +1,26 @@
+import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from "@/components/DashboardBox";
+import {
+  useGetKpisQuery,
+  useGetProductsQuery,
+  useGetTransactionsQuery,
+} from "@/state/api";
+import { DataGrid } from "@mui/x-data-grid";
 
 type Props = {};
+
 const Row3 = (props: Props) => {
+  const { data: kpiData } = useGetKpisQuery();
+  const { data: productData } = useGetProductsQuery();
+  const { data: transactionData } = useGetTransactionsQuery();
+  // console.log(transactionData);
+
   return (
     <>
-      <DashboardBox gridArea="g">"g"</DashboardBox>
+      <DashboardBox gridArea="g">
+        <BoxHeader />
+        <DataGrid />
+      </DashboardBox>
       <DashboardBox gridArea="h">"h"</DashboardBox>
       <DashboardBox gridArea="i">"i"</DashboardBox>
       <DashboardBox gridArea="j">"j"</DashboardBox>
