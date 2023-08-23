c4d2dded363d22b1fad4cd69200f9d7e0a774e1d
Laurie Roy <laurieroy.dev@gmail.com>
2023-08-23 16:42:39 -0500

    Build out prediction line charts

--- src/scenes/predictions/index.tsx
+++ src/scenes/predictions/index.tsx
@@ -1,8 +1,112 @@
-type Props = {}
+import DashboardBox from "@/components/DashboardBox";
+import FlexBetween from "@/components/FlexBetween";
+import { useGetKpisQuery } from "@/state/api";
+import { Box, Button, Tooltip, Typography, useTheme } from "@mui/material";
+import { useMemo, useState } from "react";
+import {
+  ResponsiveContainer,
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Legend,
+  Line,
+  Label,
+} from "recharts";
+
+type Props = {};
 
 const Predictions = (props: Props) => {
+  const { palette } = useTheme();
+  const [isPredictions, setIsPredictions] = useState(false);
+  const { data: kpiData } = useGetKpisQuery();
+
+  const formattedData = useMemo(() => {
+    if (!kpiData) return [];
+
+    const monthData = kpiData[0].monthlyData;
+    const formatted = monthData.map(
+      ({ month, revenue, expenses }, i: number) => {
+        return [i, revenue];
+      }
+    );
+  }, [kpiData]);
+
   return (
-    <div>Predictions</div>
-  )
-}
-export default Predictions
+    <DashboardBox width="100%" height="100%" p="1rem" overflow="hidden">
+      <FlexBetween m="1rem 2.5rem" gap="1rem">
+        <Box>
+          <Typography variant="h3">Revenue and Predictions</Typography>
+          <Typography variant="h6">
+            charted revenue and prediction revenue based on a simple linear
+            regression model
+          </Typography>
+        </Box>
+        <Button
+          onClick={() => setIsPredictions(!isPredictions)}
+          sx={{
+            color: palette.grey[900],
+            bgcolor: palette.grey[700],
+            boxShadow: "0.1rem 0.1rem 0.1rem 0.1rem rgba(0,0,0,0.4)",
+          }}
+        >
+          Show Predicted Revenue
+        </Button>
+      </FlexBetween>
+
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          data={formattedData}
+          margin={{
+            top: 20,
+            right: 75,
+            left: 20,
+            bottom: 80,
+          }}
+        >
+          <CartesianGrid stroke={palette.grey[800]} strokeDasharray="3 3" />
+          <XAxis dataKey="name" tickLine={false} style={{ fontSize: "10px" }}>
+            <Label value="Month" offset={-5} position="insideBottom" />
+          </XAxis>
+
+          <YAxis
+            domain={[12000, 26000]}
+            axisLine={{ strokeWidth: "0" }}
+            tickFormatter={(v) => `$${v}`}
+            style={{ fontSize: "10px" }}
+          >
+            <Label
+              value="Revenue in USD"
+              offset={-5}
+              angle={-90}
+              position="insideLeft"
+            />
+          </YAxis>
+          <Tooltip />
+          <Legend verticalAlign="top" />
+          <Line
+            type="monotone"
+            dataKey="Actual Revenue"
+            stroke={palette.primary.main}
+            strokeWidth={0}
+            dot={{ strokeWidth: 5 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="Regression Line"
+            stroke="#8884d8"
+            dot={false}
+          />
+          {isPredictions && (
+            <Line
+              type="monotone"
+              dataKey="Predicted Revenue"
+              stroke={palette.secondary[500]}
+            />
+          )}
+        </LineChart>
+      </ResponsiveContainer>
+    </DashboardBox>
+  );
+};
+export default Predictions;
